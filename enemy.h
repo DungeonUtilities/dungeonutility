1045db3a0a7f79da36fbc8356056d9b8d2a01e19
tristianity <tristanmillman@gmail.com>
2016-04-19 17:35:48 -0400

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -7,11 +7,11 @@ using namespace std;
 
 class Enemy {
 private:
-	int Hp, Speed, Level, EnemyType;
+	int Hp, Speed, Level, EnemyType; //Declares variables
 	int Str, Dex, Con, Int, Wis, Cha;
 	string Name, Environment;
 
-	void setType();
+	void setType();					//Declares methods
 	void setStats(int rating);
 
 public:
@@ -19,13 +19,13 @@ public:
 	void enemyStats();
 };
 
-Enemy::Enemy(int rating) {
+Enemy::Enemy(int rating) { //Creates an enemy object
 	Level = rating;
 	setType();
 	setStats(rating);
 }
 
-void Enemy::enemyStats() {
+void Enemy::enemyStats() {	//Displays stats for an enemy object
 	cout << "Level " << Level << ' ' << Name << endl;
 	cout << "Environment: " << Environment << endl;
 	cout << "Hp: " << Hp << endl;
@@ -38,7 +38,7 @@ void Enemy::enemyStats() {
 	cout << "Cha: " << Cha << endl;
 }
 
-void Enemy::setType() {
+void Enemy::setType() {	//Allows the user to select an enemy type
 	int EnemyType;
 
 	cout << "\nSelect an enemy type" << endl;
@@ -48,47 +48,42 @@ void Enemy::setType() {
 	cin >> EnemyType;
 	cout << endl;
 
-	while (true) {
-		while (cin.fail()) {
-			cin.clear();
-			cin.ignore(std::numeric_limits<int>::max(), '\n');
-			cout << "\nInvalid input!" << endl;
-			cout << "Please enter 1, 2, or 3" << endl;
-			cin >> EnemyType;
-		}
+	while (cin.fail() || EnemyType < 1 || EnemyType > 3) {	//checks for a valid input
+		cin.clear();
+		cin.ignore(std::numeric_limits<int>::max(), '\n');
+		cout << "\nInvalid input!" << endl;
+		cout << "Please enter 1, 2, or 3" << endl;
+		cin >> EnemyType;
+	}
 
-		if (EnemyType < 1 || EnemyType > 3) {
+		/*if (EnemyType < 1 || EnemyType > 3) {
 			cin.clear();
 			cout << "\nInvalid choice!" << endl;
 			cout << "Please enter 1, 2, or 3" << endl;
 			cin >> EnemyType;
+		}*/
+
+	if (EnemyType = 1) {	//generates an enemy for choice 1
+		Name = "Giant Eagle";
+		Environment = "Temperate Mountains";
+		Speed = 50;
 		}
 
-		else if (EnemyType = 1) {
-			Name = "Giant Eagle";
-			Environment = "Temperate Mountains";
-			Speed = 50;
-			break;
-		}
+	else if (EnemyType = 2) {	//generates an enemy for choice 2
+		Name = "Goblin";
+		Environment = "Temperate plains";
+		Speed = 30;
+	}
 
-		else if (EnemyType = 2) {
-			Name = "Goblin";
-			Environment = "Temperate plains";
-			Speed = 30;
-			break;
-		}
-
-		else if (EnemyType = 3) {
-			Name = "Harpy";
-			Environment = "Temperate marshes";
-			Speed = 50;
-			break;
-		}
+	else if (EnemyType = 3) {	//generates an enemy for choice 3
+		Name = "Harpy";
+		Environment = "Temperate marshes";
+		Speed = 50;
 	}
 }
 
-void Enemy::setStats(int rating) {
-	int hp = (rating / 3) * Dice::abilityScore();
+void Enemy::setStats(int rating) {	//sets the stats for an enemy object
+	int hp = (rating / 3) * Dice::abilityScore();	//sets intial stats
 	int str = Dice::abilityScore();
 	int dex = Dice::abilityScore();
 	int con = Dice::abilityScore();
@@ -98,160 +93,88 @@ void Enemy::setStats(int rating) {
 
 	int scale;
 
-	cout << "Select a difficulty" << endl;
+	cout << "Select a difficulty" << endl;	//allows user to choose a difficulty for the enemy
 	cout << "(1) Easy" << endl;
 	cout << "(2) Normal" << endl;
 	cout << "(3) Hard" << endl;
 	cin >> scale;
 
-	while (true) {
-		while (cin.fail()) {
+	while (cin.fail() || scale < 1 || scale > 3) {	//checks for valid user input
 			cin.clear();
 			cin.ignore(std::numeric_limits<int>::max(), '\n');
 			cout << "\nInvalid input!" << endl;
 			cout << "Please enter 1, 2, or 3" << endl;
 			cin >> scale;
+	}
+
+	if (scale = 3) {//Scales the stats up for a hard enemy
+		Hp = hp + Dice::abilityScore();
+		Str = str + Dice::roll(10);
+		Dex = dex + Dice::roll(10);
+		Con = con + Dice::roll(10);
+		Int = intel + Dice::roll(10);
+		Wis = wis + Dice::roll(10);
+		Cha = cha + Dice::roll(10);
+	}
+
+	else if (scale = 1) {//scales the sats down for an easy enemy
+		Hp = -1;
+		while (Hp < 1) {
+			Hp = hp - Dice::roll(10);
+		}
+		
+		Str = -1;
+		while (Str < 1) {
+			Str = str - Dice::roll(10);
 		}
 
-		if (scale < 1 || scale > 3) {
-			cin.clear();
-			cout << "\nInvalid choice!" << endl;
-			cout << "Please enter 1, 2, or 3" << endl;
-			cin >> EnemyType;
+		Dex = -1;
+		while (Dex < 1) {
+			Dex = dex - Dice::roll(10);
 		}
 
-		if (scale = 3)
-		{
-			Hp = hp + Dice::abilityScore();
-			Str = str + Dice::abilityScore();
-			Dex = dex + Dice::abilityScore();
-			Con = con + Dice::abilityScore();
-			Int = intel + Dice::abilityScore();
-			Wis = wis + Dice::abilityScore();
-			Cha = cha + Dice::abilityScore();
-
-			break;
+		Con = -1;
+		while (Con < 1) {
+			Con = con - Dice::roll(10);
+		}
+		
+		Int = -1;
+		while (Int < 1) {
+			Int = intel - Dice::roll(10);
 		}
 
-		else if (scale = 1)
-		{
-			Hp = -1;
-			while (Hp < 1)
-			{
-				Hp = hp - Dice::abilityScore();
-			}
-
-			Str = -1;
-			while (Str < 1)
-			{
-				Str = str - Dice::abilityScore();
-			}
-
-			Dex = -1;
-			while (Dex < 1)
-			{
-				Dex = dex - Dice::abilityScore();
-			}
-
-			Con = -1;
-			while (Con < 1)
-			{
-				Con = con - Dice::abilityScore();
-			}
-
-			Int = -1;
-			while (Int < 1)
-			{
-				Int = intel - Dice::abilityScore();
-			}
-
-			Wis = -1;
-			while (Wis < 1)
-			{
-				Wis = wis - Dice::abilityScore();
-			}
-
-			Cha = -1;
-			while (Cha < 1)
-			{
-				Cha = cha - Dice::abilityScore();
-			}
-
-			break;
+		Wis = -1;
+		while (Wis < 1) {
+			Wis = wis - Dice::roll(10);
 		}
 
-		else if (scale = 2)
-		{
-			Hp = hp;
-			Str = str;
-			Dex = dex;
-			Con = con;
-			Int = intel;
-			Wis = wis;
-			Cha = cha;
-
-			break;
+		Cha = -1;
+		while (Cha < 1) {
+			Cha = cha - Dice::roll(10);
 		}
 	}
-};
 
-void generateEncounter(int PartySize, int Level) {
+	else if (scale = 2)	{//does not scale stats for a normal enemy
+		Hp = hp;
+		Str = str;
+		Dex = dex;
+		Con = con;
+		Int = intel;
+		Wis = wis;
+		Cha = cha;
+	}
+}
+
+void generateEncounter(int PartySize, int Level) {	//method for generating an encounter given the party's size and the sum of the party's levels
 	int partySize = PartySize;
 	int level = Level;
 	int avgLevel = level / partySize;
 	int numEnemy;
 	int enemyLevel;
 
-	/*cout << "How many members are in your party?" << endl;
-	cin >> partySize;
-
-	while (true) {
-		while (cin.fail()) {
-			cin.clear();
-			cin.ignore(std::numeric_limits<int>::max(), '\n');
-			cout << "Invalid input!" << endl;
-			cout << "Please enter a valid party size" << endl;
-			cin >> partySize;
-		}
-
-		if (partySize < 1 || partySize > 5) {
-			cin.clear();
-			cout << "Invalid choice!" << endl;
-			cout << "You can only have up to 5 members in your party" << endl;
-			cin >> partySize;
-		}
-
-		else {
-			cout << "Enter the levels of your party members" << endl;
-			for (int i = 0; i < partySize; i++) {
-				cout << "Member " << i + 1 << ": ";
-				cin >> level;
-				while (true) {
-					while (cin.fail()) {
-						cin.clear();
-						cin.ignore(std::numeric_limits<int>::max(), '\n');
-						cout << "\nInvalid input!" << endl;
-						cout << "Please enter a valid player level" << endl;
-						cin >> level;
-					}
-
-					if (level < 1 || level > 20) {
-						cin.clear();
-						cout << "\nInvalid choice!" << endl;
-						cout << "Please enter a valid player level" << endl;
-						cin >> level;
-					}
-
-					else break;
-				}
-
-				avgLevel += level;
-			}*/
-
-	cout << "\nHow many enemies would you like to generate? ";
+	cout << "\nHow many enemies would you like to generate? ";	//Allows the user to choose how many enemies to generate
 	cin >> numEnemy;
-	//while (true) {
-	while (cin.fail() || numEnemy < 1 || numEnemy > 8) {
+	while (cin.fail() || numEnemy < 1 || numEnemy > 8) {	//Checks for valid user input
 		cin.clear();
 		cin.ignore(std::numeric_limits<int>::max(), '\n');
 		cout << "\nInvalid input!" << endl;
@@ -259,24 +182,15 @@ void generateEncounter(int PartySize, int Level) {
 		cin >> numEnemy;
 	}
 
-				/*if (numEnemy < 1 || numEnemy > 8) {
-					cin.clear();
-					cin.ignore(std::numeric_limits<int>::max(), '\n');
-					cout << "Invalid choice!" << endl;
-					cout << "You can only generate up to 8 enemies" << endl;
-					cin >> partySize;
-				}*/
+	enemyLevel = avgLevel / numEnemy; //Calculates the levels of the enemies
+	Enemy* enemy[8];	//declares an array of pointers to enemy objects
 
-				
-	enemyLevel = avgLevel / numEnemy;
-	Enemy* enemy[8];
-
-	for (int i = 0; i < numEnemy; i++) {
+	for (int i = 0; i < numEnemy; i++) {	//fills the array with enemy objects
 		cout << "Creating enemy " << i + 1 << endl;
 		enemy[i] = new Enemy(enemyLevel);
 	}
 
-	cout << "Displaying enemy stats for this encounter" << endl;
+	cout << "Displaying enemy stats for this encounter" << endl; //displays the stats for all enemies generated
 	for (int i = 0; i < numEnemy; i++) {
 		cout << "Enemy " << i + 1 << ": ";
 		enemy[i]->enemyStats();
